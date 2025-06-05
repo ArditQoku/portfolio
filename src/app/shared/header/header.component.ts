b0c26c429d4329994333672be65eddc6587d66c3
Ardit Qoku <arditqoku@hotmail.com>
2025-06-05 20:05:49 +0200

    improvements of style, routerlinks, send message in contact and picture

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ControllService } from './../../controll.service';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, TranslateModule, HttpClientModule],
+  imports: [CommonModule, TranslateModule, HttpClientModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   translate = inject(TranslateService);
   controll = inject(ControllService);
+  router = inject(Router);
+  viewportScroller = inject(ViewportScroller);
 
   switchLanguage(language: string) {
     this.controll.switchLanguage(language);
@@ -21,5 +26,17 @@ export class HeaderComponent {
 
   toggleSidebar() {
     this.controll.toggleSidebar();
+  }
+  
+  scrollToSectionOnStartpage(sectionId: string, offset: number = 0) {
+    if (this.router.url !== '/' && this.router.url !== '/start') {
+      this.router.navigate(['/']).then(() => {
+        setTimeout(() => {
+          this.controll.scrollToElement(sectionId, offset);
+        }, 100);
+      });
+    } else {
+      this.controll.scrollToElement(sectionId, offset);
+    }
   }
 }
--- src/app/shared/imprint/imprint.component.ts
+++ src/app/shared/imprint/imprint.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
